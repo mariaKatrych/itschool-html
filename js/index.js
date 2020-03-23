cf657e404207507441631815620d8d3910b325bb
mariaKatrych <mariaKatrych@gmail.com>
2020-03-23 11:00:53 +0000

    IT School

--- js/index.js
+++ js/index.js
@@ -1,23 +1,23 @@
 document.querySelector('.web').addEventListener('click', openWeb);
 
 function openWeb() {
-    window.location.replace('../itschool-html/html/web-development.html');
+    window.location.replace('../html/web-development.html');
 }
 
 document.querySelector('.db').addEventListener('click', openDB);
 
 function openDB() {
-    window.location.replace('../itschool-html/html/database.html');
+    window.location.replace('../html/database.html');
 }
 
 document.querySelector('.qa').addEventListener('click', openQA);
 
 function openQA() {
-    window.location.replace('../itschool-html/html/qa.html');
+    window.location.replace('../html/qa.html');
 }
 
 document.querySelector('.ui').addEventListener('click', openUI);
 
 function openUI() {
-    window.location.replace('../itschool-html/html/ui-design.html');
+    window.location.replace('../html/ui-design.html');
 }
